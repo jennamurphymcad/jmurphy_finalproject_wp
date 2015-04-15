6b7eb1f004571a9e5b46fda15aef2882c79c5847
Jenna Murphy <jmurphy@mcad.edu>
2015-04-15 01:06:08 -0500

    css typography, css footer/header

--- wp-content/themes/srazhalys/footer.php
+++ wp-content/themes/srazhalys/footer.php
@@ -13,9 +13,9 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		
 		  	<ul>
-            	<li><a href="https://www.facebook.com/Srazhalys">Facebook</a></li>
-            	<li><a href="http://www.reverbnation.com/srazhalys">Reverbnation</a></li>
-            	<li><a href="https://www.youtube.com/user/JCReinemann/videos">YouTube</a></li>
+            	<li><a href="https://www.facebook.com/Srazhalys"><img class="logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/facebook.png"> </a></li>
+            	<li><a href="http://www.reverbnation.com/srazhalys"><img class="logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/reverbnation.png"> </a></li>
+            	<li><a href="https://www.youtube.com/user/JCReinemann/videos"><img class="logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/youtube.png"> </a></li>
             </ul>
                <h3>&copy; Srazhalys 2014</h3>
 
